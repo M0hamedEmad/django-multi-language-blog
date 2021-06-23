e0a19153e781a526f388bb4063f89c8e85b069d7
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-23 09:44:46 +0200

    add post app

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Blog Apps
+    'post.apps.PostConfig',
 ]
 
 MIDDLEWARE = [

107d85a3af3a1ae5a37e790f076c291a64f50f16
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-26 07:10:01 +0200

    add account urls

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('account.urls', namespace='account')),
     path('', include('post.urls', namespace='post')),
 ]
 

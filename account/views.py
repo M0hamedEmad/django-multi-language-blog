14e86e18d5234e8f2bde803380f39d4d61587537
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-26 14:28:11 +0200

    add form.html

--- account/views.py
+++ account/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from .forms import UserLoginForm
 
 class LoginPage(LoginView):
     template_name = 'account/login.html'
+    # form_class = UserLoginForm
+    authentication_form = UserLoginForm
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if '?next=/' in request.get_full_path():
+                return super().get(request, *args, **kwargs)
+            else:
+                return redirect('post:home')
+        
+        return super().get(request, *args, **kwargs)

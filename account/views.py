803caa5e70ab306d095f7e0cf8353f3eca30b27e
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-26 15:40:53 +0200

    start register view

--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
 from .forms import UserLoginForm
 
 class LoginPage(LoginView):
@@ -8,6 +10,10 @@ class LoginPage(LoginView):
     # authentication_form = UserLoginForm
     
     def get(self, request, *args, **kwargs):
+        """ 
+        check if user already login redierct to home page
+        if user redierict from some place in site render login page in massage
+        """
         if request.user.is_authenticated:
             if '?next=/' in request.get_full_path():
                 return super().get(request, *args, **kwargs)
@@ -15,3 +21,10 @@ class LoginPage(LoginView):
                 return redirect('post:home')
         
         return super().get(request, *args, **kwargs)
+    
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'account/register.html'
+    
+

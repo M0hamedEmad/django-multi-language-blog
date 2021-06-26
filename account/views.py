68c8757620d1ca90f631a4caa8c09c0070ccbf92
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-26 16:19:30 +0200

    add register page

--- account/views.py
+++ account/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import FormView
 from django.contrib.auth.models import User
-from .forms import UserLoginForm
+from .forms import UserLoginForm,RegistrationForm
+
+class RegisterView(FormView):
+    model = User
+    template_name = 'account/register.html'
+    form_class = RegistrationForm
+    success_url = reverse_lazy('post:home')
+    
+    def get(self, *args, **kwargs):
+        """ 
+        check if user already login redierct to home page
+        """
+        if self.request.user.is_authenticated:
+            return redirect('post:home')
+        return super().get(*args, **kwargs)
+
+    
+    def form_valid(self, form):
+        """ 
+        override form_vaild finction to make user login after register
+        Note:
+            if you add email verification delete it    
+            
+        """
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
 
 class LoginPage(LoginView):
     template_name = 'account/login.html'
@@ -22,9 +51,3 @@ class LoginPage(LoginView):
         
         return super().get(request, *args, **kwargs)
     
-
-class RegisterView(CreateView):
-    model = User
-    template_name = 'account/register.html'
-    
-

800a05993981cb082590104ffb1fb5428a6dd61f
MohamedEmad <mohamedemad1891@gmail.com>
2021-06-30 18:06:26 +0200

    add post list view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User, Group
+from django.contrib import messages
+from post.models import Post, Comment, Category
+from account.models import Profile
+from .forms import CategoryForm, UserForm, UserUpdateForm, ProfileForm, PostForm
 
-# Create your views here.
+# Posts Views
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'dashboard/post_list.html'
+    context_object_name = 'posts'

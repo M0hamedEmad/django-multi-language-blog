8a339faf2c128c0414c7f76a734b301df29e092e
mohamed <mohamedemad1891@gmail.com>
2021-08-23 16:31:07 +0200

    collect static

--- project/settings.py
+++ project/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',

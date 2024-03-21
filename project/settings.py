5b076b8ffff45fb8b129a17c52f98aead226373b
mohamed emad <mohamedemad1891@gmail.com>
2024-03-21 21:58:43 +0200

    edit settings

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from decouple import config
 from django.utils.translation import gettext_lazy as _
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,12 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', 'True') != 'True'
 
-ALLOWED_HOSTS = ['multilanguageblog.herokuapp.com', '127.0.0.1']
+# DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['multilanguageblog.herokuapp.com', '127.0.0.1', '*'] #TODO
 
 
 # Application definition
@@ -76,14 +78,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+DEVELOPMENT_MODE = os.environ.get('DEVELOPMENT_MODE', True)
+if DEVELOPMENT_MODE == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:    
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd2phhvclmsq7ak',
-        'HOST': 'ec2-35-153-91-18.compute-1.amazonaws.com',
-        'PORT': 5432,
-        'USER': 'pnnugdgfofyznk',
-        'PASSWORD': 'fb7a2eaf31cfe2ad1e8264e1f5d711fe101d36b9e8ca8ebd9ffc1b79c96535be'
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
